2ebb6569b956744fef7cb7d57a4ac9c0f0d31d0e
stiveuz <ahmadraimov89@gmail.com>
2023-04-06 12:17:41 +0500

    new file

---
+++ src/main/java/com/example/demo/RegController.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import javafx.stage.Stage;
+
+public interface RegController {
+    void start(Stage primaryStage);
+}
---
+++ src/main/java/com/example/demo/RegistorController.java
@@ -0,0 +1,15 @@
+
+
+package com.example.demo;
+
+import javafx.stage.Stage;
+
+public class RegistorController implements RegController {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Welcome");
+
+        primaryStage.show();
+    }
+
+}
